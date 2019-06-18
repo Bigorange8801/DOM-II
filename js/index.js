cd61a2087efbf4f58c26b22ac0e5f65af6da700d
John <cjd8801@gmail.com>
2019-06-18 15:32:26 -0500

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const navTag = document.querySelector('nav');
+navTag.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 700);//turn back time
+  });
+  
+  
